570760602b2237fcb17e3579887d0fc9daf08b91
Md.Salauddin <162622583+salauddin85@users.noreply.github.com>
2024-08-21 14:51:17 +0600

    Update urls.py

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,11 +1,13 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from  .views import  UserRegistrationApiView,UserLoginApiView,activate,UserLogoutView
-router = DefaultRouter() # amader router
 from .import views
+from  .views import  UserRegistrationApiView,UserLoginApiView,activate,UserLogoutView,AccountView,ContactUsView
+router = DefaultRouter() # amader router
+router.register('account',AccountView, basename='account')
+router.register('contactus',ContactUsView, basename='contactus')
 
 urlpatterns = [
-   
+    path('', include(router.urls)),
     path('register/', UserRegistrationApiView.as_view(), name='register'),
     path('login/', UserLoginApiView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
