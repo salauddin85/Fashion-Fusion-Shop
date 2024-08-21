a237f9886dfafe88d45385e92226a72f0db4a9aa
Md.Salauddin <162622583+salauddin85@users.noreply.github.com>
2024-08-21 15:00:28 +0600

    Update models.py

--- cloth_product/models.py
+++ cloth_product/models.py
@@ -13,7 +13,85 @@ class Product(models.Model):
     quantity = models.IntegerField()
     description = models.TextField()
     size = models.CharField(max_length=100,null=True,blank=True,choices=SIZE)
+    from django.db import models
+from django.contrib.auth.models import User
+from cloth_category.models import Category,Sub_Category
+
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+
+
+from .constraints import SIZE,STAR_CHOICES
+# 1 add color 
+# color = models.CharField(max_length=50)
+class Product(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    sub_category = models.ForeignKey(Sub_Category,on_delete=models.CASCADE,null=True,blank=True)
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='cloth_product/media/images')
+    price = models.DecimalField(decimal_places=2,max_digits=10)
+    quantity = models.IntegerField()
+    description = models.TextField()
+    size = models.CharField(max_length=100,null=True,blank=True,choices=SIZE)
     
+    
+
+    
+    def __str__(self) -> str:
+        return f" Product : {self.name} , Category: {self.sub_category.name}"
+
+class Wishlist(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, blank=True)
+   
+    def __str__(self) -> str:
+        return f"{self.user.first_name}{self.user.last_name} "
+
+
+
+
+class Review(models.Model):
+    reviewer = models.ForeignKey(User,on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add = True)
+    rating = models.CharField(choices = STAR_CHOICES, max_length = 10)
+    
+    def __str__(self):
+        return f"Reviewer: {self.reviewer.first_name} {self.reviewer.last_name} "
+
+
+
+# class Review(models.Model):
+#     reviewer = models.ForeignKey(User,on_delete=models.CASCADE)
+#     body = models.TextField()
+#     name = models.CharField(max_length=50)
+#     image = models.ImageField(upload_to='cloth_product/media2/review/images')
+#     created = models.DateTimeField(auto_now_add = True)
+#     rating = models.CharField(choices = STAR_CHOICES, max_length = 10)
+    
+#     def __str__(self):
+#         return f"Reviewer: {self.reviewer.first_name} {self.reviewer.last_name} "
+
+
+
+
+
+
+
+
+
+#  def delete_products_with_wishlist(sender, instance, **kwargs):
+#         products = instance.products.all()
+#         for product in products:
+#             product.delete()
+
+# # Signal to delete the wishlist when a user is deleted
+#     @receiver(post_delete, sender=User)
+#     def delete_user_wishlist(sender, instance, **kwargs):
+#         try:
+#             instance.wishlist.delete()
+#         except Wishlist.DoesNotExist:
+#             pass
     
 
     
