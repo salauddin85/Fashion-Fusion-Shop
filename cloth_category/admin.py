ce375024a2313c22470e988ed9ddede041b1fa7f
Md.Salauddin <162622583+salauddin85@users.noreply.github.com>
2024-08-21 14:57:53 +0600

    Update admin.py

--- cloth_category/admin.py
+++ cloth_category/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Category
+from .models import Sub_Category
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
     list_display = ['name','slug']
 
+class Sub_CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+    list_display = ['name','slug']
+
 admin.site.register(Category,CategoryAdmin)
+admin.site.register(Sub_Category,Sub_CategoryAdmin)
