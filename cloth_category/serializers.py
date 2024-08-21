6d085f038edcef4718487a6833c360f65107c125
Md.Salauddin <162622583+salauddin85@users.noreply.github.com>
2024-08-21 14:54:07 +0600

    Update serializers.py

--- cloth_category/serializers.py
+++ cloth_category/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from .models import Category
+from .models import Category,Sub_Category
+
+
 class CategorySerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Category
+        fields = ['id', 'name', 'slug']
+        
+class SubCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sub_Category
         fields = '__all__'
         
