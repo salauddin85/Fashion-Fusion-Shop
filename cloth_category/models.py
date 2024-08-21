f5f84eee507aa7c2636fc8da28f68222143804dd
Md.Salauddin <162622583+salauddin85@users.noreply.github.com>
2024-08-21 14:53:40 +0600

    Update models.py

--- cloth_category/models.py
+++ cloth_category/models.py
@@ -5,5 +5,15 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField()
 
+
     def __str__(self) -> str:
         return self.name
+    
+class Sub_Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField()
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,null=True,blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
