ee225375bb6159dfd8da9c1940adb97e273523a5
Md.Salauddin <162622583+salauddin85@users.noreply.github.com>
2024-08-21 15:01:48 +0600

    Update views.py

--- cloth_product/views.py
+++ cloth_product/views.py
@@ -7,11 +7,12 @@ from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly
 from rest_framework.decorators import action
 from rest_framework import status,pagination
 from .constraints import SIZE
+from cloth_category.models import Category,Sub_Category
 from rest_framework.views import APIView
 
 
 class ProductPagination(pagination.PageNumberPagination):
-    page_size = 2 # items per page
+    page_size = 10 # items per page
     page_size_query_param = page_size
     max_page_size = 100
 
@@ -35,26 +36,100 @@ class ProductViewset(viewsets.ModelViewSet):
             products = Product.objects.filter(size=size)
         else:
             products = Product.objects.all()
+        page = self.paginate_queryset(products)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+       
         serializer = self.get_serializer(products, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
+    
+    @action(detail=False, methods=['get'], url_path='sorted_by_category/(?P<category>[^/.]+)')
+    def sorted_by_category(self, request, category):
+        try:
+            products = Product.objects.filter(sub_category__category__name=category)
+            
+            if not products.exists():
+                return Response({'error': "No Product found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        except Product.DoesNotExist:
+            return Response({'error': "No Product found"}, status=status.HTTP_404_NOT_FOUND)
+
+        page = self.paginate_queryset(products)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+       
+        serializer = self.get_serializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['get'], url_path='sorted_by_sub_category/(?P<category>[^/.]+)')
+    def sorted_by_sub_category(self, request, category):
+        try:
+            products = Product.objects.filter(sub_category__name=category)
+            
+            if not products.exists():
+                return Response({'error': "No Product found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        except Product.DoesNotExist:
+            return Response({'error': "No Product found"}, status=status.HTTP_404_NOT_FOUND)
+
+        page = self.paginate_queryset(products)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+       
+        serializer = self.get_serializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    
+    @action(detail=False, methods=['get'], url_path='sorted_by_color/(?P<color>[^/.]+)')
+    def sorted_by_color(self, request, color):
+        try:
+            products = Product.objects.filter(color=color)
+            
+            if not products.exists():
+                return Response({'error': "No Product found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        except Product.DoesNotExist:
+            return Response({'error': "No Product found"}, status=status.HTTP_404_NOT_FOUND)
+
+        page = self.paginate_queryset(products)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+       
+        serializer = self.get_serializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     
 
-    
+
+
+
+
+
     @action(detail=False, methods=['get'])
     def sorted_by_price(self, request):
         sort_order = request.query_params.get('order', 'asc')
         if sort_order == 'asc':
             products = Product.objects.all().order_by('price')
-        elif sort_order=='desc':
+        elif sort_order == 'desc':
             products = Product.objects.all().order_by('-price')
         else:
             products = Product.objects.all()
+
+        page = self.paginate_queryset(products)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
         serializer = self.get_serializer(products, many=True)
         return Response(serializer.data)
-
     
+
+
 
 
 
@@ -73,33 +148,43 @@ class WishlistViewset(viewsets.ModelViewSet):
         user = self.request.user
         if not Wishlist.objects.filter(user=user).exists():
             serializer.save(user=user)
-        else:
-            pass
 
-    @action(detail=False, methods=['post'], url_path='add_product/(?P<product_id>\d+)')#/d ??
+    @action(detail=False, methods=['post'], url_path=r'add_product/(?P<product_id>\d+)')
     def add_product(self, request, product_id=None):
         user = request.user
-        
         wishlist, created = Wishlist.objects.get_or_create(user=user)
+        
         try:
             product = Product.objects.get(id=product_id)
         except Product.DoesNotExist:
             return Response({'error': 'Product does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+        
+        if product in wishlist.products.all():
+            return Response({'status': 'Product already in wishlist.'}, status=status.HTTP_200_OK)
         
         wishlist.products.add(product)
         return Response({'status': 'Product added to wishlist.'}, status=status.HTTP_200_OK)
 
-
-    @action(detail=False, methods=['post'], url_path='remove_product/(?P<product_id>\d+)')
+    @action(detail=False, methods=['post'], url_path=r'remove_product/(?P<product_id>\d+)')
     def remove_product(self, request, product_id=None):
         user = request.user
-        wishlist, created = Wishlist.objects.get_or_create(user=user)
         try:
+            wishlist = Wishlist.objects.get(user=user)
             product = Product.objects.get(id=product_id)
+        except Wishlist.DoesNotExist:
+            return Response({'error': 'Wishlist does not exist.'}, status=status.HTTP_404_NOT_FOUND)
         except Product.DoesNotExist:
             return Response({'error': 'Product does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+        if product not in wishlist.products.all():
+            return Response({'status': 'Product not found in wishlist.'}, status=status.HTTP_200_OK)
+        
         wishlist.products.remove(product)
         return Response({'status': 'Product removed from wishlist.'}, status=status.HTTP_200_OK)
+    
+
+
+
+
 
 
 
@@ -111,3 +196,26 @@ class ReviewViewset(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(reviewer=self.request.user)
+
+
+
+
+# from auth_app.models import Account
+
+# class ReviewViewset(viewsets.ModelViewSet):
+#         queryset = Review.objects.all()
+#         serializer_class = ReviewSerializer
+#         permission_classes = [IsAuthenticatedOrReadOnly]
+
+#         def perform_create(self, serializer):
+#             user = self.request.user
+#             # Get the associated Account for the user
+#             try:
+#                 account = Account.objects.get(user=user)
+#                 name = f"{account.user.first_name} {account.user.last_name}"
+#             except Account.DoesNotExist:
+#                 # Handle case where Account does not exist
+#                 name = "Unknown Account Holder"
+            
+#             # Save the Review with the account holder's name
+#             serializer.save(reviewer=user, name=name)
