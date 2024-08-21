5458ef21a2bb8d18a7d2ddfaec25c451545bd64b
Md.Salauddin <162622583+salauddin85@users.noreply.github.com>
2024-08-21 14:54:36 +0600

    Update urls.py

--- cloth_category/urls.py
+++ cloth_category/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryApiView
+from .views import CategoryApiView,SubCategoryApiView
 router = DefaultRouter()
 
-router.register('list',CategoryApiView, basename='category')
+router.register('category_list',CategoryApiView, basename='category')
+router.register('subcategory_list',SubCategoryApiView, basename='sub_category')
 
 urlpatterns = [
     path('', include(router.urls)),
