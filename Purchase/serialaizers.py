32824dc07312546fe06cd726455bf9c64d164ed6
Md.Salauddin <162622583+salauddin85@users.noreply.github.com>
2024-08-21 15:08:00 +0600

    Update serialaizers.py

--- Purchase/serialaizers.py
+++ Purchase/serialaizers.py
@@ -1,10 +1,24 @@
 # serializers.py
 from rest_framework import serializers
-from .models import PurchaseModel
+from .models import PurchaseModel,PurchaseCartModel
 
+from cloth_product.serialaizers import ProductSerializer
 class PurchaseSerializer(serializers.ModelSerializer):
+    # product = ProductSerializer(many=True, read_only=True)
+
     class Meta:
         model = PurchaseModel
         fields = ['']
         # fields = ['product']
-        
+   
+class PurchaseCartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PurchaseCartModel
+        fields = ['']
+
+class PurchaseProductSerialaizer(serializers.ModelSerializer):
+    product = ProductSerializer( read_only=True)
+
+    class Meta:
+        model = PurchaseModel
+        fields =['id', 'user', 'product']
