ea81b44ae594bc5bd123abe017941a863318d127
Md.Salauddin <162622583+salauddin85@users.noreply.github.com>
2024-08-21 15:00:50 +0600

    Update serialaizers.py

--- cloth_product/serialaizers.py
+++ cloth_product/serialaizers.py
@@ -3,7 +3,20 @@ from .models import Product,Wishlist,Review
 from django.contrib.auth.models import User
 from .constraints import STAR_CHOICES,SIZE
 
+from rest_framework import serializers
+from .models import Product
+
+# class ProductSerializer(serializers.ModelSerializer):
+
+    # class Meta:
+    #     model = Product
+    #     fields = ['id', 'name', 'sub_category', 'image', 'price', 'description']
+
+
+
 class ProductSerializer(serializers.ModelSerializer):
+    # sub_category = serializers.CharField(source='sub_category.name')
+
     # size = serializers.MultipleChoiceField(choices = SIZE)
     # size = serializers.ListField(child=serializers.ChoiceField(choices=SIZE))
 
@@ -12,20 +25,22 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         # fields = '__all__'
 
-        fields = ['id','name','category','image','price','quantity','description','size']
+        fields = ['id','name','sub_category','image','price','quantity','description','size']
 
 
 
 
 class WishlistSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField(many=False)
     products = ProductSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Wishlist
-        fields = '__all__'
+        fields = ['id', 'user', 'products']
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    # user_name = serializers.CharField(source='user.get_full_name', read_only=True)
+
     class Meta:
         model = Review
         fields ='__all__'
