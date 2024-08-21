c0a606770763111607ede9226a222ba470aeb14a
Md.Salauddin <162622583+salauddin85@users.noreply.github.com>
2024-08-21 15:08:19 +0600

    Update urls.py

--- Purchase/urls.py
+++ Purchase/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 from .views import PurchaseProductView
 from rest_framework.routers import DefaultRouter
  
+from .views import  PurchaseCartView,PurchaseProductallView
 
-# router = DefaultRouter()
-# router.register('list',PurchaseProductView, basename='purchase')
+router = DefaultRouter()
+router.register('purchase_all',PurchaseProductallView, basename='purchase')
 urlpatterns = [
-    # path('', include(router.urls)),
-    path("list/<int:id>",PurchaseProductView.as_view(),name='purchase')
+    path('', include(router.urls)),
+    path("list/<int:id>",PurchaseProductView.as_view(),name='purchase'),
+    path("payment_cart/<str:price>",PurchaseCartView.as_view(),name='purchase'),
 
 ] 
